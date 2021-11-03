0b303576f2e94bb900b6acf55c90ccf28fb40a2b
jitendra <jitendra_shakya@hotmail.com>
2021-11-03 15:00:07 +0000

    Featurize file corrected

--- src/utils/featurize.py
+++ src/utils/featurize.py
@@ -5,14 +5,12 @@ import joblib
 import scipy.sparse as sparse
 import numpy as np
 
-
 def save_matrix(df, matrix, out_path):
     id_matrix = sparse.csr_matrix(df.id.astype(np.int64)).T
     label_matrix = sparse.csr_matrix(df.label.astype(np.int64)).T
 
-    result = spare.hstack([id_matrixm label_matrix, matrix], format="csr")
+    result = sparse.hstack([id_matrix, label_matrix, matrix], format="csr")
 
-    msg = f"The output matirx {out_path} size {result.shape} and data type: {result.dtype}"
+    msg = f"The ouput matrix {out_path} of size {result.shape} and data type: {result.dtype}"
     logging.info(msg)
-    
     joblib.dump(result, out_path)
